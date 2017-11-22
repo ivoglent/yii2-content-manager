c066438954daede91c01d870228099d8d448b2bb
Long Nguyen <>
2017-11-22 13:04:57 +0700

    Update editor stuff

--- src/widgets/views/editor.php
+++ src/widgets/views/editor.php
@@ -24,12 +24,12 @@ $editorId = substr(md5(time() . uniqid()), 0, 6);
                 height: 500,
                 menubar: false,
                 plugins: [
-                    'advlist autolink lists link image charmap print preview anchor textcolor',
+                    'advlist autolink lists link image charmap print preview anchor textcolor code',
                     'searchreplace visualblocks code fullscreen',
                     'insertdatetime media table contextmenu paste code help'
                 ],
-
-                toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                fontsize_formats: "8px 10px 12px 14px 16px 18px 20px 22px 24px 36px",
+                toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | fontsizeselect | removeformat | code | help',
                 content_css: [
                     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
                     '//www.tinymce.com/css/codepen.min.css'],
